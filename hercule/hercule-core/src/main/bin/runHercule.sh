991ed876e822427511637b08985b31b542542b8a
erara <erawan.panya@gmail.com>
2016-06-12 18:15:10 +0100

    amélioration batch & properties

--- hercule/hercule-core/src/main/bin/runHercule.sh
+++ hercule/hercule-core/src/main/bin/runHercule.sh
@@ -20,8 +20,13 @@ CODE_ERREUR_CONFIG=4
 
 
 Usage() {
-    echo "\nUsage: "
-    echo "$0 "
+	echo "----------------------------------------------------------------------------"
+    echo "	Usage: "
+    echo "		$0 import"
+    echo "		$0 export"
+    echo "		$0 itineraires"
+    echo "		$0 dump {user} {password}"
+	echo "----------------------------------------------------------------------------"
 }
 
 
@@ -37,12 +42,25 @@ fi
 #==============================================
 # Vérification des paramètres du batch
 #==============================================
-if [ $# -ne 1  ];  then
-    echo "Les paramètres de la ligne de commande ne sont pas corrects."
-    Usage
-    exit $CODE_ERREUR_USAGE
+if [ $# -eq 0 ]; then
+	Usage
+	exit $CODE_ERREUR_USAGE
+elif [ "$1" = "import" ]||[ "$1" = "export" ]||[ "$1" = "itineraires" ]; then
+	if [ $# -ne 1  ];  then
+		echo "Les paramètres de la ligne de commande ne sont pas corrects."
+		Usage
+		exit $CODE_ERREUR_USAGE
+	fi
+elif [ "$1" = "dump" ]; then
+	if [ $# -ne 3  ];  then
+		echo "Les paramètres de la ligne de commande ne sont pas corrects."
+		Usage
+		exit $CODE_ERREUR_USAGE
+	fi 
+else
+	Usage
+	exit $CODE_ERREUR_USAGE
 fi
-
 
 
 # Répertoire d'exécution du batch
@@ -52,14 +70,17 @@ DIR=`echo $( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )`
 #==============================================
 # Ajout des classes + config dans le CLASSPATH
 #==============================================
-
-for jar in `find $DIR/../lib -name "*.jar" -print`
-do
-    CLASSPATH=$jar:$CLASSPATH
-done
-
-CLASSPATH=$DIR/../config:$CLASSPATH
-
-
-
-java -Xmx256m -Xms64m -Dfile.encoding=ISO-8859-1 -classpath $CLASSPATH com.hercule.workflow.Workflow $*
+if [ "$1" = "import" ]||[ "$1" = "export" ]||[ "$1" = "itineraires" ]; then
+	for jar in `find $DIR/../lib -name "*.jar" -print`
+	do
+		CLASSPATH=$jar:$CLASSPATH
+	done
+	CLASSPATH=$DIR/../config:$CLASSPATH
+	HERCULE_DATA=$HOME/data
+	java -Xmx256m -Xms64m -Dfile.encoding=UTF-8 -DHERCULE_DATA=$HERCULE_DATA -classpath $CLASSPATH com.hercule.workflow.Workflow $*
+else
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	MY_DUMP = cat ${DIR}/../config/hercule.properties | grep ^DUMP_TABLES | awk -F= '{print $2}'
+	mysqldump -u $2 --password=$3 hercule ${MY_DUMP} > dump_metropaul_bdd.sql
+	
+fi
